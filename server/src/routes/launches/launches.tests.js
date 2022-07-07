df1eef3ded788aeac2e80f7c15c7ccd510e750a9
MNGARCIA085 <mngarcia085@gmail.com>
2022-07-07 18:55:12 -0300

    more test

--- server/src/routes/launches/launches.tests.js
+++ server/src/routes/launches/launches.tests.js
@@ -11,9 +11,40 @@ describe('Test GET/launches', () => {
  
  
 describe('Test POST/launches', () => {
-    test('....', () => {
- 
-    });
-    test('Debe validar campos obligatorios', () => {});
-    test('Debe validar el formato de fecha', () => {});
+
+
+
+    describe('Test POST /launch', () => {
+        const completeLaunchData = {
+          mission: 'USS Enterprise',
+          rocket: 'NCC 1701-D',
+          target: 'Kepler-62 f',
+          launchDate: 'January 4, 2028',
+        };
+      
+        const launchDataWithoutDate = {
+          mission: 'USS Enterprise',
+          rocket: 'NCC 1701-D',
+          target: 'Kepler-62 f',
+        };
+
+
+        test('It should respond with 201 created', async () => {
+            const response = await request(app)
+            .post('/launches')
+            .send(completeLaunchData)
+            .expect('Content-Type', /json/)
+            .expect(201);
+        })
+
+
+
+        // testeamos la fecha por separado; aquí no usamos supertest sino jest
+        const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+        const responseDate = new Date(response.body.launchDate).valueOf();
+        expect(responseDate).toBe(requestDate);
+        expect(response.body).toMatchObject(launchDataWithoutDate);
 })
+
+
+
