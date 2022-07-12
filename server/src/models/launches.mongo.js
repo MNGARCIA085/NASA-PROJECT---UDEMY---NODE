4b0cffc7f6a30db5887a395e0164d4514ad32e14
MNGARCIA085 <mngarcia085@gmail.com>
2022-07-11 21:11:59 -0300

    db

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const launchesSchema = new mongoose.Schema({
+  flightNumber: {
+    type: Number,
+    required: true,
+  },
+  launchDate: {
+    type: Date,
+    required: true,
+  },
+  mission: {
+    type: String,
+    required: true,
+  },
+  rocket: {
+    type: String,
+    required: true,
+  },
+  target: {
+    type: String,
+  },
+  customers: [ String ],
+  upcoming: {
+    type: Boolean,
+    required: true,
+  },
+  success: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+});
+
+
+/**
+ * si target fuese referenciado; hace referencia a "Planet"
+ * target:{
+ *   type: mongoose.ObjectId,
+ *   ref: 'Planet' 
+ * }
+ * 
+ *  */ 
+
+
+
+
+// Connects launchesSchema with the "launches" collection
+module.exports = mongoose.model('Launch', launchesSchema);
--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,42 @@
 const http = require('http');
+const mongoose = require('mongoose');
+
 
 const app = require('./app');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+
+const MONGO_URL = '*****';
+
+
+
 const server = http.createServer(app);
 
 
 async function startServer(){
+    await mongoose.connect(MONGO_URL);
+
+    
     await loadPlanetsData();
     server.listen(PORT, () => {
         console.log(` Listening in the port ${PORT}  `)
     })
 }
+
+
+
+// chequeo la conexión
+mongoose.connection.once('open', ()=> {
+    console.log('MongoDB ready');
+})
+
+
+// si hay errores
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+})
+
 
 startServer();
